15f0ccdf439b94f5075b2c124335115acb4bacf7
Slobodan Stojanovic <slobodan@cloudhorizon.com>
2016-02-04 12:33:49 +0100

    chore(dist): auto-publish on Github pages

---
+++ about/assets/js/main.js
@@ -0,0 +1,11 @@
+'use strict'
+
+var menu = document.getElementById('menu-button')
+  , backgroundWrapper = document.getElementById('background-wrapper')
+  , randomNumber = Math.round(Math.random() * 21) + 1
+
+backgroundWrapper.style.backgroundImage = 'url(assets/images/backgrounds/background-' + randomNumber + '.jpg)'
+
+menu.addEventListener('click', function() {
+  menu.classList.toggle('is-active')
+})
